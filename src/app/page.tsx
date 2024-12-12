ac78807544c4ce5faa198f8d6a6eb6dfa987e06b
devarshadkhan <developer.codewitharsh@gmail.com>
2024-12-12 16:46:24 +0530

    remove navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,46 +5,8 @@ import Image from 'next/image'
 const Home = () => {
   return (
     <>
-      <div className=''>
-      <nav className="py-8 flex justify-between items-center pl-6 pr-6 lg:pl-16 lg:pr-20">
-        <h1 className="text-2xl font-bold text-blue-900">Logo</h1>
-        <button
-          className="lg:hidden flex flex-col space-y-1"
-          // onClick={toggleMenu}
-        >
-           {/* <FaBars className="text-2xl text-blue-900" />  */}
-        </button>
-        <ul
-          className={` absolute lg:static top-20 left-0 w-full lg:w-auto bg-white lg:flex lg:space-x-6 font-semibold text-gray-400 z-10`}
-        >
-          <li className="p-4 lg:p-0">
-            <a href="#home" className="hover:text-blue-900">
-              Home
-            </a>
-          </li>
-          <li className="p-4 lg:p-0">
-            <a href="#about" className="hover:text-blue-900">
-              About us
-            </a>
-          </li>
-          <li className="p-4 lg:p-0">
-            <a href="#work" className="hover:text-blue-900">
-              Work
-            </a>
-          </li>
-          <li className="p-4 lg:p-0">
-            <a href="#info" className="hover:text-blue-900">
-              Info
-            </a>
-          </li>
-          <li className="p-4 lg:p-0 text-blue-900">
-            <a href="#contact" className="hover:text-gray-400">
-              Contact us
-            </a>
-          </li>
-        </ul>
-      </nav>
-
+      <div className='h-screen flex justify-center items-center'>
+    
       <section className="py-12 px-12 container mx-auto">
         <div className="  flex flex-col lg:flex-row items-center px-4 justify-between">
           <div className="lg:w-1/2 mb-8 lg:mb-0">
